2b45b838013c079dcde95967a8254ac320339c8c
elkazajo <mzhumakov@gmail.com>
2025-11-12 10:37:53 +0500

    added await method to write function

--- src/mastra/agents/story-splitter-agent.ts
+++ src/mastra/agents/story-splitter-agent.ts
@@ -5,12 +5,14 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { read, write } from 'pomljs';
 
+// ----- Обработка промпта -----
 const projectRoot = path.resolve(process.cwd().replace('.mastra\\output', ''));
 const pomlPath = path.resolve(projectRoot, 'src\\mastra\\prompt\\story-splitter-prompt.poml');
 const pomlFile = fs.readFileSync(pomlPath, 'utf-8');
 const renderedPoml = await read(pomlFile);
-const instructions = write(renderedPoml);
+const instructions = await write(renderedPoml);
 
+// ----- Создание агента -----
 export const storySplitter = new Agent({
   name: 'story-splitter',
   instructions: instructions,
